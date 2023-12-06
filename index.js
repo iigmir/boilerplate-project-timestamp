5bcb9ec270d0a0884ba81125dbef21279b029d53
iigmir <roc120j@gmail.com>
2023-12-06 13:35:01 +0800

    Add error code

--- index.js
+++ index.js
@@ -21,13 +21,26 @@ app.get("/", function(req, res) {
 
 // your first API endpoint... 
 app.get("/api/:date?", function(req, res) {
-    const get_unit = (input) => {
-        return /^\d{13}$/g.test(input) ? Number(input) : String(input)
-    };
-    const date = req.params.date ? new Date(get_unit(req.params.date)) : new Date();
+    function get_date(input = undefined) {
+        if(input) {
+            const date_str = /^\d{13}$/g.test(input) ?
+                // An unix timestamp string
+                Number(input) :
+                // A typical date string
+                String(input)
+            ;
+            return new Date( date_str );
+        }
+        return new Date();
+    }
+    const date = get_date(req.params.date);
+    const utc = date.toString();
+    if( utc === "Invalid Date" ) {
+        return { error: utc };
+    }
     res.json({
         unix: date.getTime(),
-        utc: date.toString(),
+        utc: utc,
     });
 });
 
